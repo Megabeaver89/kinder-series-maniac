bcb1c08765c53cd8828c4bd04ae7999a1c06d4b8
Megabeaver <megabeaver2024@gmail.com>
2024-09-21 14:44:43 +0700

    feat: добавить мидллвару валидации запросов

---
+++ kinder-maniac-api/middlewares/validate.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-useless-escape */
+const { celebrate, Joi } = require('celebrate')
+const mongoose = require('mongoose')
+const { regexForLink } = require('../constants/regex')
+
+const validateUserBodyForSignUp = celebrate({
+  body: Joi.object({
+    name: Joi
+      .string()
+      .min(2)
+      .max(30),
+    email: Joi
+      .string()
+      .email()
+      .required(),
+    password: Joi
+      .string()
+      .required(),
+  }),
+})
+
+const validateUserBodyForSignIn = celebrate({
+  body: Joi.object({
+    email: Joi
+      .string()
+      .email()
+      .required(),
+    password: Joi
+      .string()
+      .required(),
+  }),
+})
+
+const validateUserBodyForPatchUserInfo = celebrate({
+  body: Joi.object({
+    name: Joi
+      .string()
+      .min(2)
+      .max(30),
+    email: Joi
+      .string()
+      .email(),
+  }),
+})
+
+const validateMovieBodyForPost = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(regexForLink),
+    trailerLink: Joi.string().required().regex(regexForLink),
+    thumbnail: Joi.string().required().regex(regexForLink),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    movieId: Joi.number().required(),
+  }),
+})
+
+const validateMovieParams = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().required().custom((value, helpers) => {
+      if (!mongoose.Types.ObjectId.isValid(value)) {
+        return helpers.message('Invalid movie ID')
+      }
+      return value
+    }),
+  }),
+})
+
+module.exports = {
+  validateUserBodyForPatchUserInfo,
+  validateUserBodyForSignUp,
+  validateUserBodyForSignIn,
+  validateMovieBodyForPost,
+  validateMovieParams,
+}
