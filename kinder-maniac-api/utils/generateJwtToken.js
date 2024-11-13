ccff8b2831fc3662a7ccd8ede16f86ba6d298720
Megabeaver <megabeaver2024@gmail.com>
2024-11-13 18:28:47 +0700

    feat: создать функцию getJwt Secret

--- kinder-maniac-api/utils/generateJwtToken.js
+++ kinder-maniac-api/utils/generateJwtToken.js
@@ -1,11 +1,34 @@
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET_LOGIN, JWT_SECRET_RESET_PASSWORD } = require('../config')
 const { JWT_EXPIRATION_MAX, JWT_EXPIRATION_MIN, JWT_TOKEN_TYPE_LOGIN } = require('../constants/jwtConfig')
+const { JWT_TOKEN_TYPE_RESET_PASSWORD } = require('../constants/jwtConfig')
+
+const getJwtSecret = (type) => {
+  let secret
+  switch (type) {
+    case JWT_TOKEN_TYPE_RESET_PASSWORD:
+      secret = JWT_SECRET_RESET_PASSWORD
+      break
+    default:
+      secret = JWT_SECRET_LOGIN
+      break
+  }
+  return secret
+}
+
+const verifyJwtToken = (token, type) => {
+  try {
+    const jwtSecret = getJwtSecret(type)
+    return jwt.verify(token, jwtSecret)
+  } catch (err) {
+    throw new Error('Invalid or expired token')
+  }
+}
 
 const generateJwtToken = (userIdentifier, type = JWT_TOKEN_TYPE_LOGIN) => {
   const isLogin = type === JWT_TOKEN_TYPE_LOGIN
   const payload = isLogin ? { _id: userIdentifier } : { email: userIdentifier }
-  const jwtSecret = isLogin ? JWT_SECRET_LOGIN : JWT_SECRET_RESET_PASSWORD
+  const jwtSecret = getJwtSecret(type)
   const expiresIn = isLogin ? JWT_EXPIRATION_MAX : JWT_EXPIRATION_MIN
   return jwt.sign(payload, jwtSecret, { expiresIn })
 }
